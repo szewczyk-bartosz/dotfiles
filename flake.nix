631c2c20c75b23afbc42dd94920185cf5b597721
Bartosz Szewczyk <szewczykbartosz@protonmail.com>
2024-11-23 18:00:45 +0000

    flake stuff

--- flake.nix
+++ flake.nix
@@ -1,6 +1,19 @@
 {
   description = "my first flake";
 
-  inputs = {};
-  outputs = {};
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05"
+  };
+
+  outputs = { self, nixpkgs, ... }
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ "./configuration.nix" ];
+      }
+    };
+  };
 }
