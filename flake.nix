dbc25056b355a8ae43d3bc3e626ba0995f0d9827
Bartosz Szewczyk <szewczykbartosz@protonmail.com>
2024-11-24 00:15:28 +0000

    added home manager to the flake

--- flake.nix
+++ flake.nix
@@ -3,16 +3,27 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpks.legacyPackages.${system};
   in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system
         modules = [ ./configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      bartosz = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
